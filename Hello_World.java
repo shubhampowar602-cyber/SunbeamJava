6a94c2c3adec8a2bea74c4a21be28f696e248f47
shubhampowar <shubhampowar602@gmail.com>
2025-12-11 09:19:22 +0530

    hello

---
+++ Hello_World.java
@@ -0,0 +1,16 @@
+package JavaBasic;
+import java.util.Scanner;
+public class Hello_World {
+
+	public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World");
+		int a =sc.nextInt();
+		System.out.println(a);
+		for(int i=0;i<=8;i++) {
+			System.out.print('a');
+		}
+		
+	} 
+
+}
